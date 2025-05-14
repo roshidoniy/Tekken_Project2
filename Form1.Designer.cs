3e9dd84d72674c0b5a9db3962e623d69b7a91eec
roshidoniy <samadbek.mahmudjonov@gmail.com>
2025-05-14 14:41:12 +0500

    added Hello world

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,15 @@
-﻿namespace Tekken_Project2
+namespace Tekken_Project2
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label helloLabel;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -22,16 +23,30 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.helloLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // helloLabel
+            // 
+            this.helloLabel.AutoSize = true;
+            this.helloLabel.Location = new System.Drawing.Point(90, 90);
+            this.helloLabel.Name = "helloLabel";
+            this.helloLabel.Size = new System.Drawing.Size(100, 23);
+            this.helloLabel.TabIndex = 0;
+            this.helloLabel.Text = "Hello World";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.helloLabel);
+            this.Name = "Form1";
+            this.Text = "Tekken";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace Tekken_Project2
 {
     public partial class Form1 : Form
